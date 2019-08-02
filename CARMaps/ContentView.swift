48633638a0101a4c44e70fc0214875db6269b7a1
Cristina Saura <csaurape@everis.com>
2019-08-02 12:35:30 +0200

    Initial Commit

---
+++ CARMaps/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CARMaps
+//
+//  Created by Cristina Saura Pérez on 02/08/2019.
+//  Copyright © 2019 everis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
