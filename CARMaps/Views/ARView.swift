f6a092c2383dce0269add9c5689f48461bf5b4b6
Cristina Saura <csaurape@everis.com>
2019-08-09 11:33:43 +0200

    ADD / UPDATE [ARView] - ready to implement AR

---
+++ CARMaps/Views/ARView.swift
@@ -0,0 +1,23 @@
+//
+//  ARView.swift
+//  CARMaps
+//
+//  Created by Cristina Saura Pérez on 09/08/2019.
+//  Copyright © 2019 everis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ARView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct ARView_Previews: PreviewProvider {
+    static var previews: some View {
+        ARView()
+    }
+}
+#endif
