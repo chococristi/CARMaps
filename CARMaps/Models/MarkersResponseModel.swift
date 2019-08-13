b2cd26305603c3375e8f1678e59d2cb398b2d84a
Cristina Saura <csaurape@everis.com>
2019-08-13 09:15:29 +0200

    UDPATE [MarkerResponse] - without unnecessary code

--- CARMaps/Models/MarkersResponseModel.swift
+++ CARMaps/Models/MarkersResponseModel.swift
@@ -7,38 +7,6 @@
 //
 
 import Foundation
-
-//struct Marker {
-//    let name: String
-//    let coordinates: [Double]
-//}
-//
-//struct Markers {
-//    var markers: [Marker]
-//}
-//
-//extension Markers {
-//    init?(json: [String: Any]) {
-//
-//        guard let markersJSON = json["markers"] as? [[String: Any]] else { return nil }
-//
-//        var markersObtanied: [Marker] = []
-//
-//        for markerJSON in markersJSON {
-//
-//            if let name = markerJSON["name"] as? String,
-//                let coordinates = markerJSON["coordinates"] as? [Double] {
-//                let newMarker = Marker.init(name: name, coordinates: coordinates)
-//                markersObtanied.append(newMarker)
-//            }
-//        }
-//
-//        self.markers = markersObtanied
-//    }
-//}
-
-//------------------------------------------------------------------------------
-
 import UIKit
 import SwiftUI
 import CoreLocation
@@ -61,9 +29,6 @@ func load<T: Decodable>(_ filename: String, as type: T.Type = T.self) -> T {
 
     do {
         let decoder = JSONDecoder()
-        print("+++++++++++++++++++++++++++++++ start ")
-        print(try decoder.decode(T.self, from: data))
-        print("+++++++++++++++++++++++++++++++ fi")
         return try decoder.decode(T.self, from: data)
     } catch {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
